05c053938fa2763a7328de483e2ad33061415727
ManuelAlvarez-R <manuel.alvarez2@udea.edu.co>
2026-05-13 18:38:51 -0500

    Add files via upload
    Se crea la base para el backend

---
+++ simplex-revisado/src/test/java/UDeA/simplex_revisado/SimplexRevisadoApplicationTests.java
@@ -0,0 +1,13 @@
+package UDeA.simplex_revisado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplexRevisadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
